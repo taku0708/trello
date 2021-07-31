a548ec66b997c9ee2718f5d15b8bf63abbd49e46
taku0708 <78.tk.nghr@gmail.com>
2021-07-31 12:33:25 +0900

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="app">
+    <button v-on:click="postTweet">ツイート</button>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import firebase from "firebase";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  methods: {
+    postTweet() {
+      firebase.firestore().collection("tweets").add({
+        text: "こんにちは、ツイートの本文です。",
+      });
+    },
+  },
+};
+</script>
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXQ69v3ad2250f6QvCEIFagZ7mLXO3BUc",
+  authDomain: "trello-58da5.firebaseapp.com",
+  projectId: "trello-58da5",
+  storageBucket: "trello-58da5.appspot.com",
+  messagingSenderId: "70511575170",
+  appId: "1:70511575170:web:e21faf28d75691c401f49b",
+  measurementId: "G-GGX60YRG0L",
+};
+
+firebase.initializeApp(firebaseConfig);
